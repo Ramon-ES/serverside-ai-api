ef197001d086115bbf8b532a7a5248934619c658
Ramon Hoffman <ramon.hoffman@enversedstudios.com>
2025-05-13 13:29:03 +0200

    changed port

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use('/process', processRoute);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
